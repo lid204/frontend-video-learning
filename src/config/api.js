0bfe79815ab2906d05cdc5f805a09339d459dcb7
Phong Duong <phong@gmail.com>
2026-04-15 10:42:10 +0700

    repush FE

--- src/config/api.js
+++ src/config/api.js
@@ -1,2 +1,4 @@
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://backend-video-learning-lid204s-projects.vercel.app/api';
+const API_BASE_URL =
+  import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000/api';
+
 export default API_BASE_URL;
